51bca664859b6775306abe2914513b4c034312fd
chunjaemin <chun4582@naver.com>
2026-02-12 19:22:40 +0900

    [Config] nativewind 세팅

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,8 @@ module.exports = function (api) {
     plugins: [
       // Required for Expo Router.
       'expo-router/babel',
+      // Required for NativeWind (Tailwind-style className on RN).
+      'nativewind/babel',
       // Keep TS path aliases working at runtime (Metro).
       [
         'module-resolver',
