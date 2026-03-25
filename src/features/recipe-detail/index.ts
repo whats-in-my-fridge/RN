fab385e143e92fb37f1e9a2d126bf767025f0389
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2026-03-25 23:46:40 +0900

    chore(#84): [WIP] recipe-liked-button 찜 API 연동

--- src/features/recipe-detail/index.ts
+++ src/features/recipe-detail/index.ts
@@ -1,4 +1,5 @@
 // features/recipe-detail 레이어의 공개 배럴 엔트리
 
+export { scrapRecipe, unscrapRecipe } from "./api/scrap-recipe";
 export { useRecipeDetailQuery } from "./model/useRecipeDetail";
 export { useRecipeScrap } from "./model/useRecipeScrap";
